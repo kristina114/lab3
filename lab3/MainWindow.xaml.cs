ff57a91222870f3f054ff7c207fc1f36baac720f
Kristina Popova <kristina_14_09@vk.com>
2023-11-23 10:44:40 +0300

    Добавьте файлы проекта.

---
+++ lab3/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace lab3
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!int.TryParse(textbox.Text, out int number)) MessageBox.Show("Поле только для числа");
+            int didit1 = number / 100;
+            int didit2 = (number % 100) / 100;
+            int didit3 = number % 10;
+            int sum = didit1 + didit2 + didit3;
+            int product = didit1 * didit2 * didit3;
+            answer1.Text += sum;
+            answer.Text += product;
+        }
+    }
+}
